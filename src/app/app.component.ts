51d3c00bab28ddf3ee9613ea0448d663377390a9
RaphaelPainter <raphael.painter@imt-atlantique.net>
2023-12-11 17:23:36 +0100

    feat(auth) : ok except redirect

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,37 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
-
+import { OAuthService, NullValidationHandler, AuthConfig } from 'angular-oauth2-oidc';
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.sass'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'recipe-front-angular';
+  title = 'Tour of Heroes';
+  
+  constructor(private oauthService: OAuthService) {
+    this.configure();
+  }
+authConfig: AuthConfig = {
+    issuer: 'http://localhost:8080/realms/dive-dev',
+    redirectUri: window.location.origin ,
+    clientId: 'angular-front',
+    responseType: 'code',
+    // at_hash is not present in JWT token
+    disableAtHashCheck: true,
+    showDebugInformation: true
+  }
+  
+  public login() {
+    this.oauthService.initLoginFlow();
+  }
+  
+  public logoff() {
+    this.oauthService.logOut();
+  }
+  
+  private configure() {
+    this.oauthService.configure(this.authConfig);
+    this.oauthService.tokenValidationHandler = new  NullValidationHandler();
+    this.oauthService.loadDiscoveryDocumentAndTryLogin();
+  }
 }
