c07fc788d35ea2071300dd0505959cfc95241815
RaphaelPainter <raphael.painter@imt-atlantique.net>
2023-12-14 10:43:37 +0100

    reinit repo : auth + stencil library

--- packages/angular/projects/stencil-wrapper/src/public-api.ts
+++
@@ -1,9 +0,0 @@
-/*
- * Public API Surface of stencil-wrapper
- */
-
-import { DIRECTIVES } from './lib';
-
-export {DIRECTIVES} from './lib'
-export * from './lib/stencil-wrapper.module';
-export * from './lib/proxy';
---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app/app.module';
+
+import { defineCustomElements } from 'acme-components-sample-shirzooa';
+
+
+
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));
+
+defineCustomElements();
